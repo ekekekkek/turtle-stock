62fafdfc4953955c4a3fd570740206b97142c5bd
ekekekkek <ek3424@columbia.edu>
2025-07-26 19:17:08 +0900

    Changing logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -65,10 +65,7 @@ const Navbar = () => {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link to="/" className="flex-shrink-0 flex items-center">
-              <div className="w-8 h-8 bg-primary-600 rounded-lg flex items-center justify-center">
-                <span className="text-white font-bold text-lg">T</span>
-              </div>
-              <span className="ml-2 text-xl font-bold text-gray-900 dark:text-white">Turtle Stock</span>
+              <span className="ml-2 text-xl font-bold text-gray-900 dark:text-white">🃏🃏🃏</span>
             </Link>
           </div>
 
